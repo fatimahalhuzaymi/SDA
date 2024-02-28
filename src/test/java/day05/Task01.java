4d7e27173ea4201187ac579a70b20a9cca75d96b
dev <fatimahalhuzaymi@gmail.com>
2024-02-28 12:58:25 +0300

    trial

--- src/test/java/day05/Task01.java
+++ src/test/java/day05/Task01.java
@@ -19,25 +19,23 @@ Verify whether all 3 options given to the question can be selected.
 When each option is selected, print the following texts on the console.
      */
 @Test
-    public void task1() throws InvalidDriverException{
+    public void task1(){
     WebDriver driver = new ChromeDriver();
     driver.manage().window();
     driver.manage().timeouts().implicitlyWait(Duration.ofSeconds(10));
     driver.get("https://demoqa.com/radio-button");
 
      driver.findElement(By.id("yesRadio")).click();
-    JavascriptException js =(JavascriptExecutor) driver.getDriver();
-    ((JavascriptExecutor) js).executeScript("argements[0].click()");
+  //  JavascriptException js =(JavascriptExecutor) driver.getDriver();
+    //((JavascriptExecutor) js).executeScript("argements[0].click()");
 
     driver.findElement(By.id("noRadio")).click();
-    JavascriptException js1 =(JavascriptExecutor) driver.getDriver();
-    ((JavascriptExecutor) js1).executeScript("argements[0].click()");
+    //JavascriptException js1 =(JavascriptExecutor) driver.getDriver();
+    //((JavascriptExecutor) js1).executeScript("argements[0].click()");
 
     driver.findElement(By.id("impressiveRadio")).click();
-    JavascriptException js2 =(JavascriptExecutor) driver.getDriver();
-    ((JavascriptExecutor) js2).executeScript("argements[0].click()");
-
-
+  //  JavascriptException js2 =(JavascriptExecutor) driver.getDriver();
+    //((JavascriptExecutor) js2).executeScript("argements[0].click()");
 }
 
 
--- src/test/java/day05/Task02.java
+++ src/test/java/day05/Task02.java
@@ -1,5 +1,8 @@
 package day05;
-
+import org.openqa.selenium.By;
+import org.openqa.selenium.WebDriver;
+import org.openqa.selenium.WebElement;
+import org.openqa.selenium.chrome.ChromeDriver;
 public class Task02 {
 
     /*
@@ -11,4 +14,38 @@ public class Task02 {
      * assert that the item name is correct inside the cart
      */
 
+
+
+    public class CartTest {
+        public static void main(String[] args) {
+            // Set the path to the ChromeDriver executable
+            System.setProperty("webdriver.chrome.driver", "path_to_chromedriver");
+
+            // Create a new instance of the ChromeDriver
+            WebDriver driver = new ChromeDriver();
+
+            // Navigate to the URL
+            driver.get("https://www.saucedemo.com/v1/inventory.html");
+
+            // Add the first item to the cart
+            WebElement addItemButton = driver.findElement(By.xpath("//button[text()='Add to cart']"));
+            addItemButton.click();
+
+            // Open the cart
+            WebElement cartIcon = driver.findElement(By.className("shopping_cart_link"));
+            cartIcon.click();
+
+            // Assert that the item name is correct inside the cart
+            WebElement itemNameInCart = driver.findElement(By.className("inventory_item_name"));
+            String itemName = itemNameInCart.getText();
+            String expectedItemName = "Name of the item"; // Replace with the expected item name
+            assert itemName.equals(expectedItemName) : "Item name in the cart is not correct";
+
+            // Print a success message if the assertion passes
+            System.out.println("Item name in the cart is correct: " + itemName);
+
+            // Close the browser
+            driver.quit();
+        }
+    }
 }
